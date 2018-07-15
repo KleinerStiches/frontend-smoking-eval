e3f1c8895f254d2188979760e13bdbd3f74d33de
andy gg <andy.gg@somemail.com>
2018-07-15 15:35:33 +0200

    fourth progress advanced view missing collapse and decollapse mechanism

--- js/advanced-view/unfold-tree.js
+++ js/advanced-view/unfold-tree.js
@@ -61,29 +61,40 @@ $(document).ready(function () {
   }
 
   var build_options_question = function (question_node, question_node_next) {
+    options_id_counter += 1;
+
     $('#node-'+(question_node.code).toString())
     .append("<div id='node-"+ question_node_next.code +"'>" +
     "[" + question_node_next.code + "] " +
     `
     <div class="border border-top mb-2"></div>
-    <span class="d-block">` +
+    <h5 class="d-block">` +
     question_node_next.question +
-    `</span>
-    <div class="mb-2 radio-container">
-      {% for option in question['options'] %}
-      <input
-        type="radio"
-        name="answer"
-        value="{{ escape(option) }}"
-        checked> `+ "xyz" +`<br>
-      {% end %}
+    `</h5>
+    <div id="radio-container-` +
+    options_id_counter.toString() + '-' +
+    question_node_next.code  + `" class="mb-2 radio-container">
     </div>`
     +
     "</div>");
+
+    for (option of question_node_next.options){
+      $(
+        '#radio-container-'+
+        options_id_counter.toString() + '-' +
+        (question_node_next.code).toString())
+      .append(
+        `<input
+          type="radio"
+          name="answer"
+          value="` + option + `"
+          checked> `+ option +`<br>`
+      );
+    }
   }
 
   var expand_tree_rec = function (last_node){
-    if (!last_node)
+    if(!last_node)
     {
       return;
     }
@@ -109,8 +120,11 @@ $(document).ready(function () {
         expand_tree_rec(last_node.next_yes);
       }
       else {
-        console.log("unknown next_yes.question_type: " +
-        last_node.next_yes.question_type);
+        console.log(
+          "next_yes code: " + last_node.next_yes.code +
+          "unknown next_yes.question_type: " +
+          last_node.next_yes.question_type
+        );
       }
 
       // last node binary next no section
@@ -133,8 +147,11 @@ $(document).ready(function () {
         expand_tree_rec(last_node.next_no);
       }
       else {
-        console.log("unknown next_no.question_type: " +
-        last_node.next_no.question_type);
+        console.log(
+          "next_no code: " + last_node.next_no.code +
+          "unknown next_no.question_type: " +
+          last_node.next_no.question_type
+        );
       }
     }
     else if (
@@ -163,8 +180,11 @@ $(document).ready(function () {
       }
       else
       {
-        console.log("unknown next.question_type: " +
-        last_node.next.question_type);
+        console.log(
+          "next code: " + last_node.next.code +
+          "unknown next.question_type: " +
+          last_node.next.question_type
+        );
       }
     }
     else
@@ -172,6 +192,8 @@ $(document).ready(function () {
       console.log("other type");
     }
   }
+
+  var options_id_counter = 0
 
   var question_tree_string = $("#content-container").html().replace("\n", "").replace("\\", "");
 
@@ -182,76 +204,3 @@ $(document).ready(function () {
   root = question_tree_json;
   expand_tree_rec(root);
 });
-
-
-/*
-
-{% for question in question_tree.items() %}
-  {% if question['question_type'] == "question-binary" %}
-    <div class="mb-2">
-      <h5 class="d-block" mb-2>
-        {{ escape(question['question']) }}
-      </h5>
-      <div
-        class="btn-group btn-group-toggle mb-2"
-        role="group"
-        aria-label="Basic example"
-        data-toggle="buttons">
-
-        <label
-          class="btn btn-primary"
-          data-toggle="collapse"
-          data-target=".multi-collapse"
-          aria-expanded="false">
-          <input
-            type="radio"
-            name="answer"
-            value="Ja">
-            Ja
-          </input>
-        </label>
-
-        <label class="btn btn-primary">
-          <input
-            type="radio"
-            name="answer"
-            value="Nein"
-            checked>
-            Nein
-          </input>
-        </label>
-      </div>
-
-      <div class="m-2 collapse multi-collapse">
-        {% for question in questions %}
-          {% if question['question_type'] == "question-input" %}
-            <div class="border border-top mb-2"></div>
-            <h5 class="d-block">{{ escape(question['question']) }}</h5>
-            <div class="input-group mb-2">
-              <input
-                class="form-control"
-                type="text"
-                placeholder="{{ escape(question['placeholder']) }}"
-                name="answer">
-              </input>
-            </div>
-          {% elif question['question_type'] == "question-options" %}
-            <div class="border border-top mb-2"></div>
-            <span class="d-block">{{ escape(question['question']) }}</span>
-            <div class="mb-2 radio-container">
-              {% for option in question['options'] %}
-              <input
-                type="radio"
-                name="answer"
-                value="{{ escape(option) }}"
-                checked> {{ escape(option) }}<br>
-              {% end %}
-            </div>
-          {% end %} <!-- if elif -->
-        {% end %} <!-- for question in questions -->
-      </div>
-    </div>
-  {% end %} <!-- if question['question_type'] == "question-binary -->
-{% end %}<!-- for question in questions -->
-
-*/
