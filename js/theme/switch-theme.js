2b2dccb262e8324ce6f49c34e1c90e8a836dd027
andy gg <andy.gg@somemail.com>
2018-07-23 21:12:12 +0200

    added logic and param to summary to mark the currently active question

--- js/theme/switch-theme.js
+++ js/theme/switch-theme.js
@@ -3,12 +3,15 @@ $(document).ready(function(){
     $(".btn").toggleClass("btn-outline-dark");
     $(".navbar").toggleClass("bg-dark");
     $(".border").toggleClass("border-dark");
+    $(".list-group-item-primary").toggleClass("list-group-item-dark");
     $("body").css("background-color", "#555")
+
 
     $(".btn").toggleClass("btn-outline-primary");
     $(".navbar").toggleClass("bg-primary");
     $(".border").toggleClass("border-primary");
-    
+    $(".list-group-item-dark").toggleClass("list-group-item-primary");
+
     if($(".navbar").hasClass("bg-dark")){
       $("body").css("background-color", "#555")
     }
